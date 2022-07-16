718271dfeb6ad20b8bd6b62ae3aea8d95b27b0a9
hts0000 <hts_0000@sina.com>
2022-07-16 17:19:21 +0800

    驾驶证上传页面表单实现

--- miniprogram/pages/register/register.ts
+++ miniprogram/pages/register/register.ts
@@ -5,12 +5,51 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    licNo: '',
+    name: '',
+    genderIndex: 0,
+    genders: ['未知', '男', '女', '其他'],
+    licImgURL: undefined as string | undefined,
+    birthday: '1999-01-01',
   },
 
   // 上传驾驶证实现
   onUploadLic() {
-    console.log("upload")
+    wx.chooseImage({
+      success: (res) => {
+        if (res.tempFilePaths.length > 0) {
+          this.setData({
+            licImgURL: res.tempFilePaths[0]
+          })
+        }
+        // TODO: upload image
+        setTimeout(() => {
+          this.setData({
+            licNo: '123456',
+            name: '张三',
+            genderIndex: 1,
+            birthday: '1989-01-01',
+          })
+        }, 1000)
+      }
+    })
+  },
+
+  // 驾照认证界面-表单事件-性别表单改变实现
+  // e是点击事件发生产生的数据
+  onGenderChange(e: any) {
+    // 这个只能打印出来看那些是我们想要的数据，再选择
+    // console.log(e)
+    this.setData({
+      genderIndex: e.detail.value,
+    })
+  },
+
+  // 驾照认证界面-表单事件-出生日期改变实现
+  onBirthdayChange(e: any) {
+    this.setData({
+      birthday: e.detail.value,
+    })
   },
 
   /**
