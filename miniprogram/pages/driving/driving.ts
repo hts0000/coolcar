b5d4eee1dddbbe06b6f31aba5ff265e40155de3d
hts0000 <hts_0000@sina.com>
2022-07-18 23:19:52 +0800

    完成行车页面，行驶时间和费用更新逻辑

--- miniprogram/pages/driving/driving.ts
+++ miniprogram/pages/driving/driving.ts
@@ -1,12 +1,35 @@
+// 每秒钟0.7分钱
+const centsPerSec = 0.7
+
+// 将时间计数转换成00:00:00的格式
+function formatDuration(sec: number): string {
+  // 如果是个位数，就在前面补上'0'
+  const padString = (n: number) =>
+    n < 10 ? '0' + n.toFixed(0) : n.toFixed(0)
+  const h = Math.floor(sec / 3600)
+  sec -= h * 3600
+  const m = Math.floor(sec / 60)
+  sec -= m * 60
+  const s = Math.floor(sec)
+  return `${padString(h)}:${padString(m)}:${padString(s)}`
+}
+
+// 将分钱数转换成0.00的格式
+// cents是多少分钱
+function formatFare(cents: number): string {
+  return (cents / 100).toFixed(2)
+}
+
 // pages/driving/driving.ts
 Page({
 
   /**
    * 页面的初始数据
    */
+  timer: undefined as number | undefined,
   data: {
-    drivingTime: "01:23:45",
-    fare: "12.34",
+    elapsed: "00:00:00",
+    fare: "0.00",
     location: {
       latitude: 23.099994,
       longitude: 113.324520,
@@ -29,11 +52,25 @@ Page({
     })
   },
 
+  setupTimer() {
+    let elapsedSec = 0
+    let cents = 0
+    this.timer = setInterval(() => {
+      elapsedSec++
+      cents += centsPerSec
+      this.setData({
+        elapsed: formatDuration(elapsedSec),
+        fare: formatFare(cents),
+      })
+    }, 1000)
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
     this.setupLocationUpdator()
+    this.setupTimer()
   },
 
   /**
@@ -62,6 +99,10 @@ Page({
    */
   onUnload() {
     wx.stopLocationUpdate()
+    // 重置计时器
+    if (this.timer) {
+      clearInterval(this.timer)
+    }
   },
 
   /**
