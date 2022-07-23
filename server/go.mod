f86c76ca7467e66b2a7a73446eb27a002ae203b7
hts0000 <hts_0000@sina.com>
2022-07-23 17:07:25 +0800

    protoc的基本用法

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,18 @@
 module coolcar
 
 go 1.18
+
+require (
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.11.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0
+	google.golang.org/protobuf v1.28.0
+)
+
+require (
+	github.com/golang/glog v1.0.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220719170305-83ca9fad585f // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- server/hello.go
+++ server/hello.go
@@ -2,12 +2,61 @@ package main
 
 import (
 	"coolcar/constraints"
+	trippb "coolcar/proto/gen/go"
+	"encoding/json"
 	"fmt"
+
+	"google.golang.org/protobuf/proto"
 )
 
 func main() {
 	fmt.Println("hello world")
 	fmt.Println("sum base on generics:", sum([]float64{0.1, 0.2, 0.3}))
+
+	// generate base on protoc: protoc -I="." --go_out="paths=source_relative:gen\go" .\trip.proto
+	trip := trippb.Trip{
+		Start:       "abc",
+		End:         "cda",
+		DurationSec: 1000,
+		FeeCent:     1000,
+		StartPos: &trippb.Location{
+			Latitude:  30,
+			Longitude: 120,
+		},
+		EndPos: &trippb.Location{
+			Latitude:  35,
+			Longitude: 115,
+		},
+		PathLocations: []*trippb.Location{
+			{
+				Latitude:  110,
+				Longitude: 110,
+			},
+			{
+				Latitude:  100,
+				Longitude: 100,
+			},
+		},
+		Status: trippb.TripStatus_FINISHED,
+	}
+	fmt.Println(&trip)
+	b, err := proto.Marshal(&trip)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%x\n", b)
+	var trip2 trippb.Trip
+	err = proto.Unmarshal(b, &trip2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(&trip2)
+
+	b, err = json.Marshal(&trip2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", b)
 }
 
 func sum[T constraints.Ordered](nums []T) (ans T) {
