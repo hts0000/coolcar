0feb09408bb5a7242c757b93bcf28f7742a01a65
hts0000 <hts_0000@sina.com>
2022-11-03 21:34:07 +0800

    定义CarService的GRPC结构

--- server/gen.bat
+++ server/gen.bat
@@ -8,8 +8,14 @@ protoc --grpc-gateway_out %PBGO_OUT_DIR% --grpc-gateway_opt logtostderr=true --g
 
 protoc --go_out %PBGO_OUT_DIR% --go_opt paths=source_relative --go-grpc_out %PBGO_OUT_DIR% --go-grpc_opt paths=source_relative blob/api/gen/v1/blob.proto
 
+protoc --go_out %PBGO_OUT_DIR% --go_opt paths=source_relative --go-grpc_out %PBGO_OUT_DIR% --go-grpc_opt paths=source_relative car/api/gen/v1/car.proto
+protoc --grpc-gateway_out %PBGO_OUT_DIR% --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative car/api/gen/v1/car.proto
+
 set PBTS_BIN_DIR=..\wx\miniprogram\node_modules\.bin
 set PBTS_OUT_DIR=..\wx\miniprogram\gen\ts\auth
+..\wx\miniprogram\node_modules\.bin\pbjs -t static -w es6 car\api\gen\v1\car.proto --no-create --no-encode --no-verify --no-delimited -o %PBTS_OUT_DIR%\car_pb.js
+..\wx\miniprogram\node_modules\.bin\pbts -o %PBTS_OUT_DIR%\car_pb.d.ts %PBTS_OUT_DIR%\car_pb.js
+
 ..\wx\miniprogram\node_modules\.bin\pbjs -t static -w es6 rental\api\gen\v1\rental.proto --no-create --no-encode --no-verify --no-delimited -o %PBTS_OUT_DIR%\rental_pb.js
 ..\wx\miniprogram\node_modules\.bin\pbts -o %PBTS_OUT_DIR%\rental_pb.d.ts %PBTS_OUT_DIR%\rental_pb.js
 
